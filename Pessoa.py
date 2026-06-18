b446f0c50484602c70fef83108b84ca192378437
Roginie Almeida dos Santos <164258862+Roginie@users.noreply.github.com>
2026-06-18 19:57:37 -0300

    Corrige calcularImposto da classe base e move metodo da PessoaJuridica para o nivel correto

--- Pessoa.py
+++ Pessoa.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+
 class Endereco:
     def __init__(self, logradouro="", numero="", enderecoComercial=False):
         self.logradouro = logradouro
@@ -11,53 +12,42 @@ class Pessoa:
     def __init__(self, nome="", rendimento=0.0, endereco=None):
         self.nome = nome
         self.rendimento = rendimento
-        self.endereco = endereco
+        self.endereco = endereco if endereco is not None else Endereco()
 
-    def calcularImposto(self, rendimento):
-        return 
+    def calcularImposto(self, rendimento: float) -> float:
+        """Metodo base. Cada tipo de pessoa implementa sua propria regra."""
+        return 0.0
+
 
 class PessoaFisica(Pessoa):
-    def __init__(self, nome="", rendimento=0.0, endereco=None, cpf="", dataNascimento=""):
-        if endereco is None:
-            endereco = Endereco()
-
-        if dataNascimento is None:
-            dataNascimento = date.today()
-        
+    def __init__(self, nome="", rendimento=0.0, endereco=None, cpf="", dataNascimento=None):
         super().__init__(nome, rendimento, endereco)
-
         self.cpf = cpf
-        self.dataNascimento = dataNascimento
+        self.dataNascimento = dataNascimento if dataNascimento is not None else date.today()
 
     def calcularImposto(self, rendimento: float) -> float:
         if rendimento <= 10500:
-            return 0
-        elif 10500 < rendimento <= 30500:
+            return 0.0
+        elif rendimento <= 30500:
             return (rendimento / 100) * 5
-        elif 30500 < rendimento <= 60000:
+        elif rendimento <= 60000:
             return (rendimento / 100) * 8
         else:
             return (rendimento / 100) * 12
 
+
 class PessoaJuridica(Pessoa):
-    def __init__(self, nome="", rendimento=0.0, endereco=None, cnpj="", dataNascimento=""):
-        if endereco is None:
-            endereco = Endereco()
-
-        if dataNascimento is None:
-            dataNascimento = date.today()
-
+    def __init__(self, nome="", rendimento=0.0, endereco=None, cnpj="", dataNascimento=None):
         super().__init__(nome, rendimento, endereco)
-
         self.cnpj = cnpj
-        self.dataNascimento = dataNascimento
+        self.dataNascimento = dataNascimento if dataNascimento is not None else date.today()
 
-        def calcularImposto(self, rendimento: float) -> float:
-            if rendimento <= 1500:
-                return 0
-            elif 1500 < rendimento <= 3500:
-                return (rendimento / 100) * 2
-            elif 3500 < rendimento <= 6000:
-                return (rendimento / 100) * 3.5
-            else:
-                return (rendimento / 100) * 5
+    def calcularImposto(self, rendimento: float) -> float:
+        if rendimento <= 1500:
+            return 0.0
+        elif rendimento <= 3500:
+            return (rendimento / 100) * 2
+        elif rendimento <= 6000:
+            return (rendimento / 100) * 3.5
+        else:
+            return (rendimento / 100) * 5
